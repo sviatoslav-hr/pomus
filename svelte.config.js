85676bedae6eb0614f7b2473d6c891c289bba402
Sviatoslav <shot.ngh@gmail.com>
2025-08-13 10:20:08 +0300

    chore: add github deploy pipeline

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,16 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'dist',
+			assets: 'dist'
+		})
+	},
+	paths: {
+		// NOTE: This is necessary for GitHub Pages deployment since it serves the app from a subdirectory.
+		base: process.env.NODE_ENV === 'production' ? '/pomus' : ''
+	}
 };
 
 export default config;
